07342f93370002c12aed3188f5b0c9dece02de09
Eshwar <eshwaranand.dubey@djtcorp.in>
2020-08-13 12:56:44 +0530

    some changes

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
             <button data-html2canvas-ignore="true" @click="download">Download</button>
             <img alt="Vue logo" src="./assets/logo.png" />
             <HelloWorld ref="hello" msg="Welcome to Your Vue.js App" />
+            <HelloWorld ref="hello" msg="Welcome to Your Vue.js App" />
+            <HelloWorld ref="hello" msg="Welcome to Your Vue.js App" />
         </div>
     </div>
 </template>
@@ -20,10 +22,28 @@ export default {
     methods: {
         download() {
             window.html2canvas(this.$refs.all).then((canvas) => {
-                var imgdata = canvas.toDataURL("image/jpg");
+                var imgData = canvas.toDataURL('image/png');
+                var imgWidth = 210;
+                var pageHeight = 295;
+                var imgHeight = canvas.height * imgWidth / canvas.width;
+                var heightLeft = imgHeight;
+                var doc = new jsPDF('p', 'mm');
+                var position = 10; // give some top padding to first page
+
+                doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+                heightLeft -= pageHeight;
+
+                while (heightLeft >= 0) {
+                    position += heightLeft - imgHeight; // top padding for other pages
+                    doc.addPage();
+                    doc.addImage(imgData, 'PNG', 0 , position, imgWidth, imgHeight);
+                    heightLeft -= pageHeight;
+                }
+                doc.save( 'file.pdf');
+                /* var imgdata = canvas.toDataURL("image/jpg");
                 var doc = new jsPDF();
                 doc.addImage(imgdata, "jpg", 10, 10);
-                doc.save("hello.pdf");
+                doc.save("hello.pdf"); */
             });
         },
     },
