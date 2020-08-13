1f2f4b1ae79f739b1acd74947346db676c7de6a8
Eshwar <eshwaranand.dubey@djtcorp.in>
2020-08-13 12:13:30 +0530

    added download

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div ref="all">
+            <img alt="Vue logo" src="./assets/logo.png" />
+            <HelloWorld ref="hello" msg="Welcome to Your Vue.js App" />
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { jsPDF } from "jspdf";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        HelloWorld,
+    },
+    mounted() {
+        this.$nextTick(() => {
+            this.download();
+        });
+    },
+    methods: {
+        download() {
+            window.html2canvas(this.$refs.all).then((canvas) => {
+                var imgdata = canvas.toDataURL("image/png");
+                var doc = new jsPDF();
+                doc.addImage(imgdata, "PNG", 10, 10);
+                doc.save("hello.pdf");
+            });
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
